ea8ef0b3d5e33a1e0672f4be78e5f1acde667d7a
saritchi <s.ritchie1@hotmail.com>
2021-06-08 13:33:01 -0700

    PlayerBanner Info displays on search

--- src/Components/PlayerBanner/PlayerBanner.js
+++ src/Components/PlayerBanner/PlayerBanner.js
@@ -6,24 +6,25 @@ class PlayerBanner extends React.Component {
     //     super(props)
     // }
 
+    renderInfo() {
+        console.log(this.props.playerInfo)
+        if(this.props.playerInfo.fullName) {
+            return (
+                <div>
+                    <h2>{this.props.playerInfo.fullName} | #{this.props.playerInfo.primaryNumber}</h2>
+                    <h3>{this.props.playerInfo.primaryPosition.abbreviation} | {this.props.playerInfo.height} | {this.props.playerInfo.weight} lb | Age: {this.props.playerInfo.currentAge}</h3>
+                    <h2>{this.props.playerInfo.currentTeam.name}</h2>
+                </div>
+
+            )
+        }
+    }
+
     render() {
         return (
-            <div >
-
-                <table className="playerCard">
-                    <tbody>
-                        <tr>
-                            <td>
-                                <img width="250" src={this.props.headshotLink} alt=""></img>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <p>{this.props.playerInfo.fullName} {this.props.playerInfo.primaryNumber}</p>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div className="PlayerBanner">
+                <img width="250" src={this.props.headshotLink} alt=""></img>
+                {this.renderInfo()}
                 
             </div>
         )}
